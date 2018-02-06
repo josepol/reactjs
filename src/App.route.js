ef8b561442c9008c6c4c5cf760e15a73ba9c053c
Jose Manuel Pol <josemanuel.pol@mad.vass.es>
2018-02-06 09:39:33 +0100

    login created

--- src/App.route.js
+++ src/App.route.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import { Home } from './containers/home';
 import { Profile } from './containers/profile';
+import { Login } from './containers/login';
 
 const AppRoutes = () => (
     <Switch>
         <Route exact path='/' component={Home}></Route>
         <Route path='/profile' component={Profile}></Route>
+        <Route path='/login' component={Login}></Route>
     </Switch>
 );
 
---
+++ src/containers/login/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Login.scss';
+
+const Login = () => (
+    <div>
+        <h1>Login</h1>
+    </div>
+)
+
+export default Login;
