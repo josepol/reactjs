7306e8ac67a29e37cf598f49466c9441f5e8013f
Jose Manuel Pol <josemanuel.pol@mad.vass.es>
2018-02-06 09:46:35 +0100

    corrected

--- src/containers/login/Login.js
+++ src/containers/login/Login.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Login.scss';
 
-const Login = () => (
-    <div>
-        <h1>Login</h1>
-    </div>
-)
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Login</h1>
+            </div>
+        );
+    }
+}
 
 export default Login;
